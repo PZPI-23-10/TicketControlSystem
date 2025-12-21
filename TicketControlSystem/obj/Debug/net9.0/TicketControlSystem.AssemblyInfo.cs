a24cb950b483e891bde96cbd3de0d77ed23348a5
Samenpien602 <KiyakoKiyakoS@gmail.com>
2025-12-21 19:45:55 +0200

    usecors

--- TicketControlSystem/obj/Debug/net9.0/TicketControlSystem.AssemblyInfo.cs
+++ TicketControlSystem/obj/Debug/net9.0/TicketControlSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketControlSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d736ba70111599883f74e2d3fe0bbdb7fa11248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e020cd25f81350cfcf1568dbd78908c30b5c6c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketControlSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketControlSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
